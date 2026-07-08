fad54463b7d8bcd54d656950ded2fbc0aaf3fa34
Justin Solomon <justinroyselsolomon@gmail.com>
2026-07-08 19:22:36 +0800

    feature(landing page): init main section file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import Image from "next/image";
 import Link from "next/link";
 import NavBar from "@/components/ui/NavBar";
 import Footer from "@/components/ui/Footer";
+import MainSection from "@/components/ui/Main";
 
 export default function Home() {
   return (
     <>
     <NavBar />
+    <MainSection />
     <Footer />
     </>
   );
