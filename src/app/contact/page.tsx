2cd35a3fb6da1476ea35d40dd7ef26a14c90e9ec
Justin Solomon <justinroyselsolomon@gmail.com>
2026-07-10 17:39:43 +0800

    fix: contact list hover states

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -10,9 +10,9 @@ export default function Contact() {
         <div className="flex flex-col justify-start items-center text-brown-500 p-5 min-h-screen">
             <h1 className="text-3xl md:text-5xl">Contact Me</h1>
             <p>Email: justinroyselsolomon@gmail.com</p>
-            <p>Facebook: <Link href="https://www.facebook.com/JustinRoyse.Solomon">JustinRoyse.Solomon</Link></p>
-            <p>LinkedIn: <Link href="https://www.linkedin.com/in/justin-royse-solomon">Justin Royse Solomon</Link></p>
-            <p>GitHub: <Link href="https://github.com/Justinroyse">Justinroyse</Link></p>
+            <p className="hover:text-blue-600">Facebook: <Link href="https://www.facebook.com/JustinRoyse.Solomon">JustinRoyse.Solomon</Link></p>
+            <p className="hover:text-blue-400">LinkedIn: <Link href="https://www.linkedin.com/in/justin-royse-solomon">Justin Royse Solomon</Link></p>
+            <p className="hover:text-gray-400">GitHub: <Link href="https://github.com/Justinroyse">Justinroyse</Link></p>
             <p>Mobile Number: 09082022037</p>
         </div>
         <Footer />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 // Landing Page
 
-import Image from "next/image";
-import Link from "next/link";
 import NavBar from "@/components/ui/NavBar";
 import Footer from "@/components/ui/Footer";
 import MainSection from "@/components/ui/Main";
